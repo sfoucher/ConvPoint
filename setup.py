bffd888c3c509480a9e9478dafae5f4edcd13f28
sfoucher <samuel.foucher@gmail.com>
2020-04-25 20:23:13 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ConvPoint-Dev',
+    version='0.1',
+    packages=['utils', 'networks', 'convpoint', 'convpoint.nn', 'convpoint.nn.legacy'],
+    url='https://github.com/sfoucher/ConvPoint',
+    license='',
+    author='fouchesa',
+    author_email='',
+    description='Modifications of convpoint'
+)
