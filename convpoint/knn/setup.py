64bc798f343853eaae02c83f6d46f20787b6a96a
sfoucher <samuel.foucher@gmail.com>
2020-04-26 09:20:55 -0400

    fixing imports

--- convpoint/knn/setup.py
+++ convpoint/knn/setup.py
@@ -2,16 +2,15 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
-
-
+import sys
 
 ext_modules = [Extension(
        "nearest_neighbors",
        sources=["knn.pyx", "knn_.cxx",],  # source file(s)
        include_dirs=["./", numpy.get_include()],
-       language="c++",            
-       extra_compile_args = [ "-std=c++11", "/openmp",],
-       extra_link_args=["-std=c++11", '/openmp /VERBOSE'],
+       language="c++",
+       extra_compile_args = [ "-std=c++11"]+ (["/openmp"] if sys.platform.startswith("win") else ["-fopenmp"]),
+       extra_link_args= [ "-std=c++11"]+ (["/openmp"] if sys.platform.startswith("win") else ["-fopenmp"]),
   )]
 
 setup(
